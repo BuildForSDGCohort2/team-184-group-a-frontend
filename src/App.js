186b98f9cf22d5aa99b7b24f8ef1d6317336501b
SAWAMWA <smlmwangi@gmail.com>
2020-09-06 15:22:30 +0300

    navbar feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css';
-
+import Header from './Components/Header'
 function App() {
   return (
     <div className="App">
-     mazao product
-     
+    
+   < Header></Header>
     </div>
   );
 }
