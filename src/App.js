5604be2c13f3b754e46c4ab4999cc941c94b11cf
SAWAMWA <smlmwangi@gmail.com>
2020-09-07 22:46:34 +0300

    new feature added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import  "./App.css";
 import Header from   "./Components/Header"
+import Landing from   "./Components/Landing"
 function App() {
   return (
     <div className="App">
-    
-   < Header></Header>
+   < Header/>
+   <Landing/>
     </div>
   );
 }
