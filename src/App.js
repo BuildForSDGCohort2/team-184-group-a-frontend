4736cc6080e26e966358e3a78e1a9a2fcc550867
SAWAMWA <smlmwangi@gmail.com>
2020-09-07 14:51:57 +0300

    removed quote

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Header from './Components/Header'
-import './App.css';
+import React from 'react'
+import Header from   "./Components/Header"
+import   "./App.css"
 
 function App() {
   return (
