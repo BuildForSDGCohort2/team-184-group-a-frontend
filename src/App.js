9852b0955625ece449e9da19d3fd6ac8aab311e8
SAWAMWA <smlmwangi@gmail.com>
2020-09-07 14:29:54 +0300

    removed quotes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import './App.css';
-import Header from './Components/Header'
+import  "./App.css";
+import Header from   "./Components/Header"
 function App() {
   return (
     <div className="App">
