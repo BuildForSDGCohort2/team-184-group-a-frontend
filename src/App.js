3ee379bb2be3100dec8bccfc348c28966b77744e
SAWAMWA <smlmwangi@gmail.com>
2020-09-12 18:28:22 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import  "./App.css";
 import Header from   "./Components/Header";
 import Landing from   "./Components/Landing";
+
+
 function App() {
   return (
     <div className="App">
    < Header/>
    <secction><Landing/></secction>
-   
+
     </div>
   );
 }
