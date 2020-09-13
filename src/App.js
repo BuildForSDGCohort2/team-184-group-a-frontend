7b3bc1ee8b6ee9c24ab9025d20eab165d7efc44c
SAWAMWA <smlmwangi@gmail.com>
2020-09-13 12:12:35 +0300

    develop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import Header from "./Components/Header";
-import Register from "./Components/Register";
-import ProductsForm from "./Components/common/ProductsForm";
-import { Footer } from "./Components/layout/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <ProductsForm />
-      <Register/>
-      <Footer />
+      <Header/>
     </div>
   );
 }
