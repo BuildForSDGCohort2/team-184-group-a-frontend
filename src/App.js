e907199fefcac59e6b7a2c141841d760fb702e60
Amakoye <charlesamakoye@gmail.com>
2020-09-14 13:48:47 +0300

    tested Product data capture form, refactored code to previous version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import  "./App.css";
-import Header from   "./Components/Header";
-import Landing from   "./Components/Landing";
-
+import "./App.css";
+import Header from "./Components/Header";
+import Landing from "./Components/Landing";
+//import ProductForm from "./Components/common/ProductForm";
 
 function App() {
   return (
     <div className="App">
-   < Header/>
-   <secction><Landing/></secction>
-
+      <Header />
+      {/*  <ProductForm /> */}
+      <section>
+        <Landing />
+      </section>
     </div>
   );
 }
